0db784e5c79ff4b59d704b1c4b9f5f787ed7bf2f
pedro tavares <pedrotavaresbol1@gmail.com>
2024-09-30 03:23:29 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,36 @@
+import { useState } from 'react'
+import './App.css'
+import { Card } from './components/card/card';
+import { FoodData } from './interface/FoodData';
+import { useFoodData } from './hooks/useFoodData';
+import { CreateModal } from './components/card/createModal/create-modal';
+
+function App() {
+
+  const { data } = useFoodData();
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const handleOpenModal = () => {
+    setIsModalOpen(prev => !prev)
+  }
+
+  return (
+    <div className="container">
+      <h1>Cardapio</h1>
+      <div className="card-grid">
+          {data?.map(foodData =>
+          <Card 
+          key={foodData.id}
+          price={foodData.price}
+          title={foodData.title}
+          image={foodData.image}
+          />
+          )}
+      </div>
+          {isModalOpen && <CreateModal closeModal={handleOpenModal}/>}
+          <button onClick={handleOpenModal}>novo</button>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/card/card.tsx
@@ -0,0 +1,17 @@
+import "./card.css"
+    
+interface CardProps {
+    price: number,
+    title: string,
+    image: string
+}
+
+export function Card(props : CardProps){
+    return(
+        <div className="card">
+            <img src={props.image} alt={props.title}></img>
+            <h2>{props.title}</h2>
+            <p><b>Valor:</b>{props.price}</p>
+        </div>
+    )
+}
