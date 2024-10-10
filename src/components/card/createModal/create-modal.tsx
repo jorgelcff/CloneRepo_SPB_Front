e80a4e6021d1e517db104e3f073d246f471b809b
Pedro Tavares <pedrotavaresbol1@gmail.com>
2024-10-10 11:17:33 -0300

    Update create-modal.tsx

--- src/components/card/createModal/create-modal.tsx
+++ src/components/card/createModal/create-modal.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react"
 import { useFoodDataMutate } from "../../../hooks/useFoodDataMutate";
+import { FoodData } from "../../../interface/FoodData";
 import "./Modal.css";
 
 interface InputProps {
