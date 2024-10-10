74e6e304fe73a3f759d044f3d39a2c5763d22752
Pedro Tavares <pedrotavaresbol1@gmail.com>
2024-10-10 11:13:14 -0300

    Update create-modal.tsx

--- src/components/card/createModal/create-modal.tsx
+++ src/components/card/createModal/create-modal.tsx
@@ -26,7 +26,7 @@ export function CreateModal({ closeModal }: ModalProps){
     const [title, setTitle] = useState("");
     const [price, setPrice] = useState(0);
     const [image, setImage] = useState("");
-    const { mutate, isSuccess, isLoading } = useFoodDataMutate();
+    const { mutate, isSuccess } = useFoodDataMutate();
 
     const submit = () => {
         const foodData: FoodData = {
@@ -53,7 +53,7 @@ export function CreateModal({ closeModal }: ModalProps){
                     <Input label="image" value={image} updateValue={setImage}/>
                 </form>
                 <button onClick={submit} className="btn-secundary">
-                    {isLoading ? 'postando...':'postar'}
+                    postar
                 </button>
             </div>
         </div>
