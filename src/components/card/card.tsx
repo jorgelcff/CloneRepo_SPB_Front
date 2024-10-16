2401a7cfe492bf7e085f247117ab89a49bd21b0f
Pedro Tavares <pedrotavaresbol1@gmail.com>
2024-10-16 10:51:50 -0300

    NameClass

--- src/components/card/card.tsx
+++ src/components/card/card.tsx
@@ -10,7 +10,7 @@ export function Card(props : CardProps){
     return(
         <div className="card">
             <img src={props.image} alt={props.title}></img>
-            <h3  class="title">{props.title}</h3>
+            <h3  className="title">{props.title}</h3>
             <p class="price"><b>Valor: </b>{props.price}</p>
         </div>
     )
