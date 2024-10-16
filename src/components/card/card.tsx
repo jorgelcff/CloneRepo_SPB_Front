a03cf01f507c86f41803264d55cb8b44dc838f57
Pedro Tavares <pedrotavaresbol1@gmail.com>
2024-10-16 10:19:32 -0300

    Alt_imagem_complete

--- src/components/card/card.tsx
+++ src/components/card/card.tsx
@@ -10,8 +10,8 @@ export function Card(props : CardProps){
     return(
         <div className="card">
             <img src={props.image} alt={props.title}></img>
-            <h2>{props.title}</h2>
-            <p><b>Valor: </b>{props.price}</p>
+            <h3  class="title">{props.title}</h3>
+            <p class="price"><b>Valor: </b>{props.price}</p>
         </div>
     )
 }
