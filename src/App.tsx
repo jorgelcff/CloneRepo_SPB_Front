ccb4fb00bbe4ab66ede715bdd9af4bc33c774a1a
Pedro Tavares <pedrotavaresbol1@gmail.com>
2024-10-18 09:43:52 -0300

    Alteração-de-código-BMQ

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,43 @@ function App() {
 
   return (
     <div className="container">
-      <h1>Bem Me Quer</h1>
+      <a href="#" className="logo-container">
+          <img src="https://front-bmq.vercel.app/midia/logo2.png" className="logo" alt="Logo Bem me quer"/>
+      </a>
+      <section id="header">
+        <div className="navbar-container">
+            <ul id="navbar">
+                <li><a href="htmls/skincare.html">Skincare</a></li>
+                <li><a href="htmls/pele.html">Pele</a></li>
+                <li><a href="htmls/olhos.html">Olhos</a></li>
+                <li><a href="htmls/boca.html">Boca</a></li>
+                <li><a href="htmls/vegan.html">Vegan</a></li>
+            </ul>
+        </div>
+        <div className="navbar-container">
+            <form className="search-form" action="#" method="get">
+                <input type="text" className="search-input" placeholder="Pesquisar..."/>
+            </form>
+        </div> 
+    </section>
+    <section className="promo-section background-image">
+        <div className="promo-content">
+            <h1 className="promo-title">Melu</h1>
+            <div className="promo-copy">
+                <div className="t-h4">
+                    <p>
+                        <b>
+                            Cuide da Sua pele
+                        </b>
+                    </p>
+                </div>
+                <div className="promo-link-group">
+                    <a aria-label="Get Started Link" href="htmls/" className="promo-link">Ver mais</a>
+                    <a aria-label="Get Started Link" href="htmls/" className="promo-link">Melu</a>
+                </div>
+            </div>
+        </div>
+    </section>
       <div className="card-grid">
           {data?.map(foodData =>
           <Card 
